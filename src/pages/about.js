d0fb9105dd58fabad5dd9744e32a945eaddfa60f
Yashdeep Jain <yashuofficial7@gmail.com>
2024-06-16 02:45:20 +0530

    Added education and experience page

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,8 @@ import aboutPic from '../../public/images/profile/developer2.png'
 import Image from 'next/image'
 import { useInView, useMotionValue, useSpring } from 'framer-motion'
 import Skills from '@/components/Skills'
+import Experience from '@/components/Experience'
+import Education from '@/components/Education'
 
 
 const AnimatedNumbers = ({value}) => {
@@ -87,6 +89,8 @@ const about = () => {
                     </div>
 
                 <Skills />
+                <Experience />
+                <Education />
                 </Layout>
             </main>
         </>
